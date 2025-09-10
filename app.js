9cb2023f5a058a71a870bfc0ea4cb35bdbc7e1b2
Vinson <vinsonhe2@gmail.com>
2025-09-10 11:40:40 -0700

    chore: set up EJS template engine
    Define the path to where the view files are and sets EJS as the view
    engine

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import path from "node:path";
 import { indexRouter } from "./routes/indexRouter.js";
 import { categoryRouter } from "./routes/categoryRouter.js";
 import { produceRouter } from "./routes/produceRouter.js";
 
 const app = express();
+
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 app.use("/categories", categoryRouter);
