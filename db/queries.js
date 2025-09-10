101ac32cd98de5bfe535106fdcc7b5031f045ddc
Vinson <vinsonhe2@gmail.com>
2025-09-10 12:00:45 -0700

    chore: set up database pool and queries

---
+++ db/queries.js
@@ -0,0 +1,13 @@
+import { pool } from "./pool.js";
+
+const getAllCategories = async () => {};
+
+const getCategory = async () => {};
+
+const insertCategory = async () => {};
+
+const getAllProduce = async () => {};
+
+const getProduce = async () => {};
+
+const insertProduce = async () => {};
