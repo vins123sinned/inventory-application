9e53fb80c28a1f31e9154ff8fe00ad2001661f98
Vinson <vinsonhe2@gmail.com>
2025-09-14 19:23:28 -0700

    style(heading): add transition and polish

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -2,5 +2,6 @@ const button = document.querySelector(".menu-button");
 const dropdown = document.querySelector(".header-nav");
 
 button.addEventListener("click", () => {
-  dropdown.classList.toggle("hidden");
+  const isOpen = dropdown.classList.toggle("open");
+  button.setAttribute("aria-expanded", isOpen);
 });
