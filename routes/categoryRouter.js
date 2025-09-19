3929dcd05b87efb2a288375282e7041f3c55adf9
Vinson <vinsonhe2@gmail.com>
2025-09-19 08:41:31 -0700

    feat(categories): set up route

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {
-  getAllCategories,
+  getCategoriesPage,
   getCategoryForm,
   getCategoryPage,
 } from "../controllers/categoryController.js";
 
 const categoryRouter = Router();
 
-categoryRouter.get("/", getAllCategories);
+categoryRouter.get("/", getCategoriesPage);
 categoryRouter.get("/:categoryId", getCategoryPage);
 categoryRouter.get("/create", getCategoryForm);
 
