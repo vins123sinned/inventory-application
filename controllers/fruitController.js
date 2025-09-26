b1e09195f0e29c74a18767655c54b0434dbcf0f8
Vinson <vinsonhe2@gmail.com>
2025-09-26 09:58:27 -0700

    feat(form): set up validation for fruit form
    - Install express-validator
    - Create validators for fruit form fields
    - Set up controller for fruit form POST method

--- controllers/fruitController.js
+++ controllers/fruitController.js
@@ -1,3 +1,4 @@
+import { body, validationResult } from "express-validator";
 import {
   getAllCategories,
   getAllFruits,
@@ -6,6 +7,37 @@ import {
   getHarvestFromId,
   getAllHarvests,
 } from "../db/queries.js";
+
+//trim whitespace too!
+const requiredErr = "is required";
+const lengthError = (maxLength) =>
+  `must be between 1 and ${maxLength} characters`;
+const priceErr = "must be a number up to 9999.99 with max 2 decimal places";
+
+const validateFruit = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage(`Name ${requiredErr}`)
+    .isLength({ min: 1, max: 255 })
+    .withMessage(`Name ${lengthError(255)}`),
+  body("price")
+    .trim()
+    .notEmpty()
+    .withMessage(`Price ${requiredErr}`)
+    .matches(/^(\d{1,4}(\.\d{1,2})?|\.\d{1,2})$/)
+    .withMessage(`Price ${priceErr}`),
+  body("image_link")
+    .trim()
+    .optional()
+    .isLength({ min: 1, max: 1000 })
+    .withMessage(`Image link ${lengthError(1000)}`),
+  body("description")
+    .trim()
+    .optional()
+    .isLength({ min: 1, max: 1000 })
+    .withMessage(`Description ${lengthError(1000)}`),
+];
 
 const getFruitsPage = async (req, res) => {
   const fruits = await getAllFruits();
@@ -41,4 +73,27 @@ const getFruitForm = async (req, res) => {
   });
 };
 
-export { getFruitsPage, getFruitPage, getFruitForm };
+const postFruitForm = [
+  validateFruit,
+  async (req, res) => {
+    const errors = validationResult(req);
+    const categories = await getAllCategories();
+    const harvests = await getAllHarvests();
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("layout", {
+        title: "Add a fruit",
+        path: "partials/fruitForm.ejs",
+        categories,
+        harvests,
+      });
+    }
+
+    const { name, price, image_link, harvest, category, description } =
+      req.body;
+    // Query here
+    res.redirect("/");
+  },
+];
+
+export { getFruitsPage, getFruitPage, getFruitForm, postFruitForm };
