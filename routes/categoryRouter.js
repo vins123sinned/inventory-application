9be9ae839a05b73a03f57f5cef5e988c04217c90
Vinson <vinsonhe2@gmail.com>
2025-09-30 10:12:59 -0700

    feat(form): set up category form POST route

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -3,12 +3,14 @@ import {
   getCategoriesPage,
   getCategoryForm,
   getCategoryPage,
+  postCategoryForm,
 } from "../controllers/categoryController.js";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", getCategoriesPage);
 categoryRouter.get("/create", getCategoryForm);
+categoryRouter.post("/create", postCategoryForm);
 categoryRouter.get("/:categoryId", getCategoryPage);
 
 export { categoryRouter };
