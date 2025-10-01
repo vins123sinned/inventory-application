c758b29f5ea0bdd5dc2b04d0d9750c9e6307d94b
Vinson <vinsonhe2@gmail.com>
2025-10-01 11:26:15 -0700

    feat(harvest): add route to edit harvest

--- routes/harvestRouter.js
+++ routes/harvestRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  getEditHarvestForm,
   getHarvestForm,
   getHarvestPage,
   getHarvestsPage,
@@ -11,6 +12,7 @@ const harvestRouter = Router();
 harvestRouter.get("/", getHarvestsPage);
 harvestRouter.get("/create", getHarvestForm);
 harvestRouter.post("/create", postHarvestForm);
+harvestRouter.get("/edit/:harvestId", getEditHarvestForm);
 harvestRouter.get("/:harvestId", getHarvestPage);
 
 export { harvestRouter };
