e00423c6f6c2ccffbb8c94cc4e7a3b072fd621ed
Vinson <vinsonhe2@gmail.com>
2025-10-01 11:48:52 -0700

    feat(harvest): add route to handle edit POST

--- routes/harvestRouter.js
+++ routes/harvestRouter.js
@@ -4,6 +4,7 @@ import {
   getHarvestForm,
   getHarvestPage,
   getHarvestsPage,
+  postEditHarvestForm,
   postHarvestForm,
 } from "../controllers/harvestController.js";
 
@@ -13,6 +14,7 @@ harvestRouter.get("/", getHarvestsPage);
 harvestRouter.get("/create", getHarvestForm);
 harvestRouter.post("/create", postHarvestForm);
 harvestRouter.get("/edit/:harvestId", getEditHarvestForm);
+harvestRouter.post("/edit/:harvestId", postEditHarvestForm);
 harvestRouter.get("/:harvestId", getHarvestPage);
 
 export { harvestRouter };
