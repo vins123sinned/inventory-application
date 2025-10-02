63453ec35953dd1d8f853c4fd95f54f64a7128fb
Vinson <vinsonhe2@gmail.com>
2025-10-02 12:11:56 -0700

    feat(fruit): add route to edit fruit

--- routes/FruitRouter.js
+++ routes/FruitRouter.js
@@ -4,6 +4,7 @@ import {
   getFruitPage,
   getFruitForm,
   postFruitForm,
+  getEditFruitForm,
 } from "../controllers/fruitController.js";
 
 const fruitRouter = Router();
@@ -11,6 +12,7 @@ const fruitRouter = Router();
 fruitRouter.get("/", getFruitsPage);
 fruitRouter.get("/create", getFruitForm);
 fruitRouter.post("/create", postFruitForm);
+fruitRouter.get("/edit/:fruitId", getEditFruitForm);
 fruitRouter.get("/:fruitId", getFruitPage);
 
 export { fruitRouter };
